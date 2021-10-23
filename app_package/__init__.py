c561b706475dd95afda07071f0d3662ecb26d5a1
Marc Gravel <gravel.m@outlook.com>
2021-10-23 16:17:37 -0700

    initialized application

---
+++ app_package/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#imports below the flask (app) initialization to avoid circular imports 
+#this is done when importing any files within this package
