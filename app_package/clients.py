538810852adb86ecec5058022eac98983d9ea737
Marc Gravel <gravel.m@outlook.com>
2021-11-03 18:28:29 -0700

    client/PATCH completed

--- app_package/clients.py
+++ app_package/clients.py
@@ -193,7 +193,76 @@ def api_clients():
                     status=201)
 
     elif request.method == 'PATCH':
-        pass
+        #only managers/admin can update client info.
+        data = request.json
+        token = data.get("sessionToken")
+        client_id = data.get("clientId")
+
+        #check id exists
+        if client_id != None:
+            id_valid = db_fetchone_index("SELECT EXISTS(SELECT id FROM clients WHERE id=?)", [client_id])
+            if id_valid == 0:
+                return Response("clientId does not exist", mimetype="text/plain", status=400)
+        else:
+            return Response("No clientId sent", mimetype="text/plain", status=400)
+
+        #check valid session token and gets auth level
+        auth_level = get_auth(token)
+        if auth_level == "invalid":
+            return Response("Invalid session Token", mimetype="text/plain", status=400)
+
+        if auth_level != "manager" and auth_level != "admin":
+            return Response("Not authorized to update client", mimetype="text/plain", status=401)
+
+        if len(data.keys()) >= 3 and len(data.keys()) <= 7:
+            upd_client = pop_dict_req(data)
+        else:
+            return Response("Invalid amount of data sent", mimetype="text/plain", status=400)
+
+        if "name" in upd_client:
+            if not check_length(upd_client["name"], 1, 60):
+                return Response("Name must be between 1 and 60 characters", mimetype="text/plain", status=400)
+            db_commit("UPDATE clients SET name=? WHERE id=?", [upd_client["name"], client_id])
+
+        if "company" in upd_client:
+            if not check_length(upd_client["company"], 0, 100):
+                db_commit("DELETE FROM clients WHERE id=?", [client_id])
+                return Response("Max 100 characters for company", mimetype="text/plain", status=400)
+            db_commit("UPDATE clients SET company=? WHERE id=?", [upd_client["company"], client_id])
+
+        if "address" in upd_client:
+            if not check_length(upd_client["address"], 0, 100):
+                db_commit("DELETE FROM clients WHERE id=?", [client_id])
+                return Response("Max 100 characters for address", mimetype="text/plain", status=400)
+            db_commit("UPDATE clients SET address=? WHERE id=?", [upd_client["address"], client_id])
+
+        if "email" in upd_client:
+            if not check_email(upd_client["email"]):
+                db_commit("DELETE FROM clients WHERE id=?", [client_id])
+                return Response("Invalid email address", mimetype="text/plain", status=400)
+            if not check_length(upd_client["email"], 0, 50):
+                db_commit("DELETE FROM clients WHERE id=?", [client_id])
+                return Response("email length max 50 characters", mimetype="text/plain", status=400)
+            db_commit("UPDATE clients SET email=? WHERE id=?", [upd_client["email"], client_id])
+
+        if "phone" in upd_client:
+            if not check_length(upd_client["phone"], 0, 20):
+                db_commit("DELETE FROM clients WHERE id=?", [client_id])
+                return Response("Phone number must be between 7 and 20 characters", mimetype="text/plain", status=400)
+            db_commit("UPDATE clients SET phone=? WHERE id=?", [upd_client["phone"], client_id])
+
+        #get updated client from db and send back in a valid dict
+        req_upd_client = db_fetchone("SELECT * FROM clients WHERE id=?", [client_id])
+
+        #create return dict and send as list
+        client_to_list = []
+        client = pop_client_all(req_upd_client)
+        client_to_list.append(client)
+        
+        return Response (json.dumps(client_to_list),
+                    mimetype="application/json",
+                    status=201)
+
     elif request.method == 'DELETE':
         pass
     else:
